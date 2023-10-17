10b074f7c5434f7ebb98035f1f326fc1361c9e8f
GyancarlosPinto <gyancarlospinto@pursuit.org>
2023-10-17 17:00:11 -0400

    Functions Written Out in Index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+const {
+    getItemById,
+    getAllPurchases,
+    createPurchase,
+    updatePurchase,
+    deletePurchase,
+    savePurchase
+} = require("./src/spookysupplies")
+
+function processInput() {
+    const expectedCommand = process.argv[2];
+    console.log(expectedCommand);
+    
+    let result = "Error: Command not found";
+
+    const item = {};
+    const customerPurchase = (process.argv.slice(3).map((value) => value.split(`=`)));
+
+    for (const details of customerPurchase) {
+        item[details[0]] = details[1];
+    }
+
+    if (expectedCommand === "create") {
+        result = createPurchase(item)
+    }
+
+    else if (expectedCommand === "get") {
+        if (item.id) {
+            result = getItemById(item.id);
+        }
+        else {
+            result = getAllPurchases();
+        }
+    }
+
+    else if (expectedCommand === "update") {
+        result = updatePurchase(item.id, item)
+    }
+
+    else if (expectedCommand === "delete") {
+        result = deletePurchase(item.id);
+    }
+
+    savePurchase();
+    console.log(result);
+}
+
+
+
+processInput();
