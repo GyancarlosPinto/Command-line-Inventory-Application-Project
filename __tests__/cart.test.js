91751c485a478fab86b2d03790419a4e733dc2e9
GyancarlosPinto <gyancarlospinto@pursuit.org>
2023-10-19 18:17:39 -0400

    Added jest.mock('fs') to use mock files for test

--- __tests__/cart.test.js
+++ __tests__/cart.test.js
@@ -1,10 +1,17 @@
+jest.mock('fs')
+
+const fs = require("../__mocks__/fs");
 const cartAPI = require("../src/cart");
 
-const cartData = require("../data/cart.json");
-
 describe("Cart API", () => {
+    describe("gets price of item or items in the cart()", () => {
+        it("correctly gets total price of all items in cart.", () => {
+          expect(cartAPI.getPriceFromCart()).toEqual("$12276.50")
+        })
+    })
+
     describe("returns same item as inputted into cart()", () => {
-        it("correctly gets details of item in persistent data", () => {
+        it("correctly gets details of item in persistent data.", () => {
           const value = {
             "itemName": "Vampire the Masquerade All Hollows Eve Collector's Silverware Dining Set",
             "priceInCents": 607500,
